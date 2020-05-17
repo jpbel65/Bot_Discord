6795d653f2b4fe5b457ba524f06fcb58e5711c97
vitrimel@gmail.com <vitrimel@gmail.com>
2020-05-17 19:46:33 -0400

    respond

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ const app = express()
 const connected = [];
 const PORT = process.env.PORT || 8080
 let countReady = 0
+let flux
 
 bot.on('ready', ()=>{
 	console.log('ready');
@@ -16,12 +17,22 @@ bot.on('ready', ()=>{
 
 bot.on('message', async function (message) {
     if (`${message}`[0] === '$') {
-		console.log((new Date()) +` ${message}`.substr(1));
-		let url = `${message}`.substr(1);
-		connected.forEach(function (item, index, array) {
-			item.sendUTF(url);
-		})
+    	if(`${message}` === '$log') {
+    		flux = message.channel;
+    		await flux.send('Log Activate')
+		}
+    	else {
+			console.log((new Date()) +` ${message}`.substr(1));
+			let url = `${message}`.substr(1);
+			connected.forEach(function (item, index, array) {
+				item.sendUTF(url);
+			})
+		}
     }
+});
+
+bot.on('ready', () => {
+
 });
 
 const server = http.createServer(app);
@@ -36,9 +47,20 @@ wsServer.on('request', function(request) {
 	let connection = request.accept(null, request.origin);
 	console.log((new Date()) + ' Connection accepted.');
 	connected.push(connection);
+	if (flux !== undefined){
+		flux.send(`LOG: ${new Date()} -: new user => length = ${connected.length}`)
+	}
 	console.log(connected.length);
 	connection.on('message', function incoming(message) {
 		console.log(message.utf8Data)
+		if(flux !== undefined){
+			flux.send(`LOG: ${new Date()} -: ${message.utf8Data}`)
+		}
+		if(message.utf8Data.contains('http')){
+			connected.forEach(function (item, index, array) {
+				item.sendUTF(message.utf8Data);
+			})
+		}
 		if(message.utf8Data === 'ready' ){
 			countReady++
 			console.log(`WebSocket message received: ${message.utf8Data} countReady: ${countReady}`);
@@ -48,6 +70,9 @@ wsServer.on('request', function(request) {
 					item.sendUTF('play');
 				})
 				console.log((new Date()) + ' : play ');
+				if (flux !== undefined){
+					flux.send(`LOG: ${new Date()} -: play send to all user`)
+				}
 			}
 		}
 	});
@@ -56,6 +81,9 @@ wsServer.on('request', function(request) {
 		var pos = connected.indexOf(connection)
 		connected.splice(pos,1)
 		console.log(connected.length)
+		if (flux !== undefined){
+			flux.send(`LOG: ${new Date()} -: lost user => length = ${connected.length}`)
+		}
 	});
 	connection.timer=setInterval(function timeout() {
 		connection.ping();
